fac51c80100182fcbfd7834759c869246330d058
Sreekanth S <sreekanthsanthosh17@gmail.com>
2026-06-06 17:16:19 +0530

    completed basic Front end UI

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -3,7 +3,7 @@ import HeroSection from "../../components/home/HeroSection";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col">
+    <div className="min-h-dvh bg-black text-white flex flex-col">
       <HeroSection />
       <PromptBar />
     </div>
